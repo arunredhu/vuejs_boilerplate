81502434799f8cca19e3fc9b189db9d4d48b3d22
Arun Redhu <arun.redhu05@gmail.com>
2019-06-13 23:58:35 +0530

    [ADD]: ErrorBoundary component created

---
+++ src/app/shared/components/error-boundary/error-boundary.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <slot
+      v-if="err"
+      name="error"
+      v-bind:err="err"
+      v-bind:vm="vm"
+      v-bind:info="info"
+    >Something went wrong</slot>
+    <slot v-else></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error-boundary",
+  data() {
+    return {
+      err: false,
+      vm: null,
+      info: null
+    };
+  },
+  errorCaptured(err, vm, info) {
+    this.err = err;
+    this.vm = vm;
+    this.info = info;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+
